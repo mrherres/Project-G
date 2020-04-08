a47252e62d1aee1a6115e2750cd4907525c30961
mrherres <t.r.heeres@student.rug.nl>
2020-04-08 16:13:43 +0200

    Created the honeycomb style.

--- project/gui.py
+++ project/gui.py
@@ -33,25 +33,41 @@ def check_score(answer, root):
 
 
 def create_text_icon(a_word, root):
-        icon0 = Text(root, height=1, width=1)
-        icon0.grid(row=2, column=2)
+        icon0 = Text(root, height=1, width=1, font='Arial 30', bg='pink')
+        icon0.grid(row=1, column=1, sticky='w', padx=15)
         icon0.insert(INSERT, a_word[3])
         icon0.insert(END, "")
 
-        icon1 = Text(root, height=1, width=1)
-        icon1.grid(row=2, column=3)
+        icon1 = Text(root, height=1, width=1, font='Arial 30', bg='pink')
+        icon1.grid(row=1, column=1, sticky='w', padx=45)
         icon1.insert(INSERT, a_word[0])
         icon1.insert(END, "")
 
-        icon2 = Text(root, height=1, width=1)
-        icon2.grid(row=2, column=4)
+        icon2 = Text(root, height=1, width=1, font='Arial 30', bg='pink')
+        icon2.grid(row=2, column=1, sticky='w')
         icon2.insert(INSERT, a_word[2])
         icon2.insert(END, "")
 
-        icon3 = Text(root, height=1, width=1)
-        icon3.grid(row=2, column=5)
+        icon3 = Text(root, height=1, width=1, font='Arial 30', bg='pink')
+        icon3.grid(row=2, column=1, sticky='w', padx=30)
         icon3.insert(INSERT, a_word[1])
         icon3.insert(END, "")
+
+        icon4 = Text(root, height=1, width=1, font='Arial 30', bg='pink')
+        icon4.grid(row=2, column=1, sticky='w', padx=60)
+        icon4.insert(INSERT, a_word[4])
+        icon4.insert(END, "")
+
+        icon5 = Text(root, height=1, width=1, font='Arial 30', bg='pink')
+        icon5.grid(row=3, column=1, sticky='w', padx=15)
+        icon5.insert(INSERT, a_word[6])
+        icon5.insert(END, "")
+
+        icon6 = Text(root, height=1, width=1, font='Arial 30', bg='pink')
+        icon6.grid(row=3, column=1, sticky='w', padx=45)
+        icon6.insert(INSERT, a_word[5])
+        icon6.insert(END, "")
+
 
 if __name__ == "__main__":
     
@@ -61,13 +77,14 @@ if __name__ == "__main__":
     answer_list = []
     
     entry_field = Entry(root)
-    entry_field.grid(row=3, column=1)
+    entry_field.grid(row=4, column=1)
     entry_field.focus()
     
     button1 = Button(root, command = get_input)
-    button1.grid(row=3, column=6)
+    button1.grid(row=4, column=6)
 
-    a = create_text_icon("blij", root)
+    a = create_text_icon("obscure", root)
+
 
     root.mainloop()
 
